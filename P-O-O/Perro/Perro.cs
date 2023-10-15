bc703e4a5faf77620012d9154a5e0f77f43005f2
TITO <117479455+Titosky21@users.noreply.github.com>
2023-10-15 16:18:01 -0300

    Perro.cs

---
+++ P-O-O/Perro/Perro.cs
@@ -0,0 +1,17 @@
+class Perro{
+
+    //Atributos
+    private string nombre;
+    private string raza;
+    private int edad;
+    private int kilos;
+
+    //Métodos
+    public void asignarNombre(string name){
+        nombre = name;
+    }
+    public string obtenerNombre(){
+        return nombre;
+    }
+  
+}

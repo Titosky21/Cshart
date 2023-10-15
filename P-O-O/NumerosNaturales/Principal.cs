e895baed661fc5ac9143e680ae572fef1263b3b7
TITO <117479455+Titosky21@users.noreply.github.com>
2023-10-15 16:05:16 -0300

    Principal.cs

---
+++ P-O-O/NumerosNaturales/Principal.cs
@@ -0,0 +1,17 @@
+using System;
+//using POO_NumNaturales;
+
+namespace POO_NumNaturales{
+    public class Principal{
+        public static void Main(string[] args){
+            NumerosN n1 = new NumerosN();
+
+        	n1.Suma(20);
+        	n1.Resta(5);
+    	    n1.Multiplicacion(3);
+        	n1.esPar();
+	        n1.esPrimo();
+
+        }
+    }
+}

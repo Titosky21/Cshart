2f4907fde3a9774f7df3a6f1912fbc6f12a01f2f
TITO <117479455+Titosky21@users.noreply.github.com>
2023-10-16 22:24:46 -0300

    Enteros.cs

---
+++ P-O-O/NúmerosComplejos/Enteros.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace POO_Complejos{
+    class Entero{
+        //Atributos
+        private int valor ;
+        
+        //Constructores
+        public Entero(){
+
+        }
+
+        public Entero(int valor){
+            this.valor = valor;
+        }
+
+        //Método
+        public int getValor(){
+            return this.valor;
+        }
+    }
+}

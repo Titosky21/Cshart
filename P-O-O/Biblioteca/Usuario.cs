e0a223bb5a1eb840112b3b21fd97424b83b5d013
TITO <117479455+Titosky21@users.noreply.github.com>
2023-10-19 00:48:08 -0300

    Usuario.cs

--- P-O-O/Biblioteca/Usuario.cs
+++ P-O-O/Biblioteca/Usuario.cs
@@ -1,34 +1,45 @@
 using System;
 
 namespace POO_Lab_tipoPrueba{
-    class Empleado{
-        //Atributos 
+    class Usuario{
+        //Atributos
         private string nombre;
         private int numeroIdentificador;
-        private string cargo;
+        private List<Abs_Biblioteca> elementosPrestados; //creamos un elemento que solo aceptara objetos tipo "Abs_Biblioteca" osea libros o revistas
 
-        //Csontructor
-        public Empleado(string nombre, int id, string cargo){
+        //Constructor
+        public Usuario(string nombre, int id){
             this.nombre = nombre;
             this.numeroIdentificador = id;
-            this.cargo = cargo;
+            this.elementosPrestados = new List<Abs_Biblioteca>(); //creamos una Lista tipo Abs_Biblioteca vacía, esté no se pide por parametro
         }
 
-        //Métodos
+        //Métodos get
         public string getNombre{
             get { return this.nombre; }
         }
 
-        public int getId{
+        public int getIdentificacion{
             get { return this.numeroIdentificador; }
         }
-        public string getCargo{
-            get { return this.cargo; }
+
+        public List<Abs_Biblioteca> getElementosPrestados{
+            get { return this.elementosPrestados; }
         }
 
+        //métodos set
+        public void prestarElemento(Abs_Biblioteca elemento){
+            elementosPrestados.Add(elemento); //al crear 'listas' del tipo "Abs_Biblioteca" estas obtienen las propiedades de las Listas
+        }
+        public void devolverElemento(Abs_Biblioteca elemento){
+            elementosPrestados.Remove(elemento);
+        }
         public void MostrarInformacion(){
-            Console.WriteLine($"Empleado: {this.nombre}, Número de Identificación: {this.numeroIdentificador}, Cargo: {this.cargo} ");
+            Console.WriteLine($"Nombre: {this.nombre}, Número de Identificación: {this.numeroIdentificador}");
+            Console.WriteLine($"Elementos Prestados: ");
+            foreach(var elemento in elementosPrestados){//el "foreach" es para buscar un tipo de elemento dentro de una lista. 
+                elemento.MostrarInformacion();
+            }
         }
-
     }
 }

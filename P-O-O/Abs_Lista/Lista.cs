b23005d08e7c8599303b82295c1752ae1af03c8d
TITO <117479455+Titosky21@users.noreply.github.com>
2023-10-20 00:29:52 -0300

    Lista.cs

---
+++ P-O-O/Abs_Lista/Lista.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace POO_Abs_Lista{
+    abstract class Lista{
+        //Métodos Abstractos 
+        public abstract void Primero();
+        public abstract void Ultimo();
+    }
+}
